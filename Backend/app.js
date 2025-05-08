2354bded326f89598eeb7a1e1101b0b43fd4ff43
Sandeep Rudola <sandeeprudola10@gmail.com>
2025-05-08 14:48:08 +0530

    Revert "Anant branch"

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,30 @@
-const dotenv = require('dotenv');
+const dotenv=require('dotenv');
 dotenv.config();
-const express = require('express');
-const server = require('./server'); // Import the server
-const app = server.app; // Get the Express app from server.js
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const connectodb = require('./db/db');
-const userroutes = require('./routes/user.routes');
-const captainroutes = require('./routes/captain.routes');
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const cookieParser=require('cookie-parser');
+const connectodb=require('./db/db');
+const userroutes=require('./routes/user.routes');
+const captainroutes=require('./routes/captain.routes');
+
 connectodb();
+
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.get('/', (req, res) => {
+
+
+
+
+
+
+app.get('/',(req,res)=>{
     res.send('hello');
-});
-app.use('/users', userroutes);
-app.use('/captains', captainroutes);
-module.exports = app;
+})
+app.use('/users',userroutes);
+app.use('/captains',captainroutes);
+
+module.exports=app;
